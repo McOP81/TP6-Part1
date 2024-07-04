f30e3ec92ed2fff74f982d28e4c912a34a404e1f
R-ELKADAH <rachidelkadah@email.com>
2024-07-04 18:03:06 +0100

    TP6-Part1

---
+++ src/test/java/ma/fsm/tp6part1/Tp6Part1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fsm.tp6part1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp6Part1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
